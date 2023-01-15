9bb847e15f575bac1d75feaf451a53bf48eadd18
Rafał Hrabia <rafixese@gmail.com>
2023-01-15 10:12:53 +0100

    Add Routers

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 urlpatterns = [
-    # path('/', ),
+    path('', include(router.urls)),
 ]
